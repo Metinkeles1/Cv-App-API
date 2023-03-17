338cb54282d2427ac6cdeeaf133ec01440a4456a
Metin Keleş <the.metin9@gmail.com>
2023-03-17 11:30:51 +0300

    AddViewModel

---
+++ Core/CvAPI.Application/ViewModels/Educations/Vm_Create_Education.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvAPI.Application.ViewModels.Educations
+{
+    public class Vm_Create_Education
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string SubTitle2 { get; set; }
+        public string GPA { get; set; }
+        public string Date { get; set; }
+    }
+
+}
---
+++ Core/CvAPI.Application/ViewModels/Educations/Vm_Update_Education.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvAPI.Application.ViewModels.Educations
+{
+    public class Vm_Update_Education
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string SubTitle2 { get; set; }
+        public string GPA { get; set; }
+        public string Date { get; set; }
+    }
+}
